3d5d437c8aa868ffcc40862f73fd07702964388c
y_zavorotynskyi <ezavor111@gmail.com>
2023-10-15 14:47:57 +0200

    reviews

--- src/Components/Reviews/Reviews.js
+++ src/Components/Reviews/Reviews.js
@@ -11,16 +11,28 @@ const Reviews = () => {
                 <span className={s.wrapper__content__span}>Відгуки клієнтів</span>
                 <div className={s.wrapper__content_rewiewBlock}>
                     <div className={s.wrapper__content_rewiewBlock__layer}>
-                        <img src={messageImgLBig} className={s.wrapper__content_rewiewBlock__layer_imgBig}/>
-                        <img src={messageImgRSmall} className={s.wrapper__content_rewiewBlock__layer_imgSmall}/>
+                        <div className={s.wrapper__content_rewiewBlock__layer_BigL}>
+                            <img src={messageImgLBig} className={s.wrapper__content_rewiewBlock__layer_imgBig}/>
+                        </div>
+                        <div className={s.wrapper__content_rewiewBlock__layer_SmallR}>
+                            <img src={messageImgRSmall} className={s.wrapper__content_rewiewBlock__layer_imgSmall}/>
+                        </div>
                     </div>
                     <div className={s.wrapper__content_rewiewBlock__layer}>
-                        <img src={messageImgLSmall} className={s.wrapper__content_rewiewBlock__layer_imgSmall}/>
-                        <img src={messageImgRBig} className={s.wrapper__content_rewiewBlock__layer_imgBig}/>
+                        <div className={s.wrapper__content_rewiewBlock__layer_SmallL}>
+                            <img src={messageImgLSmall} className={s.wrapper__content_rewiewBlock__layer_imgSmall}/>
+                        </div>
+                        <div className={s.wrapper__content_rewiewBlock__layer_BigR}>
+                            <img src={messageImgRBig} className={s.wrapper__content_rewiewBlock__layer_imgBig}/>
+                        </div>
                     </div>
                     <div className={s.wrapper__content_rewiewBlock__layer}>
-                        <img src={messageImgLBig} className={s.wrapper__content_rewiewBlock__layer_imgBig}/>
-                        <img src={messageImgRSmall} className={s.wrapper__content_rewiewBlock__layer_imgSmall}/>
+                        <div className={s.wrapper__content_rewiewBlock__layer_BigL}>
+                            <img src={messageImgLBig} className={s.wrapper__content_rewiewBlock__layer_imgBig}/>
+                        </div>
+                        <div className={s.wrapper__content_rewiewBlock__layer_SmallR}>
+                            <img src={messageImgRSmall} className={s.wrapper__content_rewiewBlock__layer_imgSmall}/>
+                        </div>
                     </div>
                 </div>
             </div>
