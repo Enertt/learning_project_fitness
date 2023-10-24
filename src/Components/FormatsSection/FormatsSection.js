56feb13e6a3b8d4e4bd6b25ce505382be1328b47
y_zavorotynskyi <ezavor111@gmail.com>
2023-10-24 19:27:12 +0200

    style fixes

--- src/Components/FormatsSection/FormatsSection.js
+++ src/Components/FormatsSection/FormatsSection.js
@@ -1,6 +1,6 @@
 import s from './formatsSection.module.css'
 import foodImg from '../../assets/images/food_scale_2.jpg'
-import online from '../../assets/images/image.psd (1).jpg'
+import online from '../../assets/images/online.jpg'
 import body from '../../assets/images/image_body.jpg'
 import trainer from '../../assets/images/trainer.jpg'
 import { useInView } from 'react-intersection-observer';
@@ -17,26 +17,27 @@ const FormatsSection = () => {
             <div className={s.wrapper__content}>
                 <span className={s.wrapper__content__spanTitle}>Виберіть свій формат</span>
 
-                <div className={inView ? s.wrapper__content__cardsBlock2 : s.wrapper__content__cardsBlock__hidden}>
-                    <div className={s.wrapper__content__cardsBlock__card2}>
-                        <div className={s.wrapper__content__cardsBlock__card__imgBlock}>
-                            <img className={s.wrapper__content__cardsBlock__card__imgBlock__img} src={foodImg} />
+                <div ref={ref} className={inView ? s.wrapper__content__cardsBlock2 : s.wrapper__content__cardsBlock__hidden}>
+                    <div className={s.wrapper__content__cardsBlock2__card}>
+                        <div className={s.wrapper__content__cardsBlock2__card__imgBlock}>
+                            <img className={s.wrapper__content__cardsBlock2__card__imgBlock__img} src={body} />
                         </div>
-                        <div className={s.wrapper__content__cardsBlock__card__textBlock}>
-                            <span className={s.wrapper__content__cardsBlock__card__spanTitle}>«Створення раціону харчування»</span>
-                            <span className={s.wrapper__content__cardsBlock__card__spanDescription}>-для схуднення</span>
-                            <span className={s.wrapper__content__cardsBlock__card__spanDescription}>-для набору</span>
-                            <span className={s.wrapper__content__cardsBlock__card__spanDescription}>-для людей з порушеннями</span>
+                        <div className={s.wrapper__content__cardsBlock2__card__textBlock}>
+                            <span className={s.wrapper__content__cardsBlock2__card__spanTitle}>«Онлайн-ведення»</span>
+                            <span className={s.wrapper__content__cardsBlock2__card__spanDescription}>-досконало підібрана під вас система тренувань</span>
+                            <span className={s.wrapper__content__cardsBlock2__card__spanDescription}>-все підкріплено відео</span>
+                            <span className={s.wrapper__content__cardsBlock2__card__spanDescription}>-звʼязок з вами 24/7</span>
+                            <span className={s.wrapper__content__cardsBlock2__card__spanDescription}>-підібрана система харчування для досягнення цілі</span>
                         </div>
-                        <div className={s.wrapper__content__cardsBlock__card__orderBlock}>
-                            <span className={s.wrapper__content__cardsBlock__card__orderBlock__cost}>2499 грн.</span>
-                            <button className={s.wrapper__content__cardsBlock__card__orderBlock__button}>Замовити</button>
+                        <div className={s.wrapper__content__cardsBlock2__card__orderBlock}>
+                            <span className={s.wrapper__content__cardsBlock2__card__orderBlock__cost}>2499 грн.</span>
+                            <button className={s.wrapper__content__cardsBlock2__card__orderBlock__button}>Замовити</button>
                         </div>
 
                     </div>
                 </div>
 
-                <div ref={ref} className={inView ? s.wrapper__content__cardsBlock : s.wrapper__content__cardsBlock__hidden}>
+                <div className={inView ? s.wrapper__content__cardsBlock : s.wrapper__content__cardsBlock__hidden}>
 
                     <div className={s.wrapper__content__cardsBlock__card}>
                         <div className={s.wrapper__content__cardsBlock__card__imgBlock}>
@@ -73,14 +74,13 @@ const FormatsSection = () => {
                     </div>
                     <div className={s.wrapper__content__cardsBlock__card}>
                         <div className={s.wrapper__content__cardsBlock__card__imgBlock}>
-                            <img className={s.wrapper__content__cardsBlock__card__imgBlock__img} src={body} />
+                            <img className={s.wrapper__content__cardsBlock__card__imgBlock__img} src={foodImg} />
                         </div>
                         <div className={s.wrapper__content__cardsBlock__card__textBlock}>
-                            <span className={s.wrapper__content__cardsBlock__card__spanTitle}>«Онлайн-ведення»</span>
-                            <span className={s.wrapper__content__cardsBlock__card__spanDescription}>-досконало підібрана під вас система тренувань</span>
-                            <span className={s.wrapper__content__cardsBlock__card__spanDescription}>-все підкріплено відео</span>
-                            <span className={s.wrapper__content__cardsBlock__card__spanDescription}>-звʼязок з вами 24/7</span>
-                            <span className={s.wrapper__content__cardsBlock__card__spanDescription}>-підібрана система харчування для досягнення цілі</span>
+                            <span className={s.wrapper__content__cardsBlock__card__spanTitle}>«Створення раціону харчування»</span>
+                            <span className={s.wrapper__content__cardsBlock__card__spanDescription}>-для схуднення</span>
+                            <span className={s.wrapper__content__cardsBlock__card__spanDescription}>-для набору</span>
+                            <span className={s.wrapper__content__cardsBlock__card__spanDescription}>-для людей з порушеннями</span>
 
                         </div>
                         <div className={s.wrapper__content__cardsBlock__card__orderBlock}>
