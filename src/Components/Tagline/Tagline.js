f554dbcaedb3cd520ab7394dbe751886ebb352f5
y_zavorotynskyi <ezavor111@gmail.com>
2023-12-31 15:50:23 +0100

    fix

--- src/Components/Tagline/Tagline.js
+++ src/Components/Tagline/Tagline.js
@@ -6,7 +6,7 @@ const Tagline = () => {
         <div className={s.wrapper}>
             <div className={s.wrapper__content}>
                 <div className={s.wrapper__content__titleBlock}>
-                    <span className={s.wrapper__content__spanTitle}>Хочеш побудувати тіло своєї мрії, чи піднятися на вершину у спотривній кар'єрі?</span>
+                    <span className={s.wrapper__content__spanTitle}>Хочеш побудувати тіло своєї мрії, чи піднятися на вершину у спортивній кар'єрі?</span>
                     <span className={s.wrapper__content__spanTitle2}>Я допоможу тобі!</span>
                 </div>
                 <div className={s.wrapper__content__swiperBlock}>
